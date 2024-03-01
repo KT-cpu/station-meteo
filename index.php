f729b5aaa27b6b9d1944b7166df485aac864c344
NathanEstay <nathan.estay@gmail.com>
2024-03-01 16:30:17 +0100

    1.0.1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  require("./db_config.php");
+  require("../conf/db_config.php");
 ?>
 
 
@@ -12,7 +12,6 @@
 
 
     <link rel="stylesheet" href="style/global.css">
-    <link rel="script" href="script.js">
     <!-- Boxicon CDN Link -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   </head>
@@ -37,29 +36,18 @@
         <ul class="nav_list">
           <li>
             <a>
-              <i class='bx bx-search' ></i>
-              <form action="" method="get">
-                <input type="text" name='ville' id="ville" placeholder="Search...">
-                <input type="submit" hidden="hidden" > 
-              </form>
+              <i class='bx bx-search' id="search"></i>
+              <input type="text" placeholder="Search..." id="inputt">
             </a>
           </li>
           <li>
-            <a href="index.html">
+            <a href="index.php">
               <i class='bx bxs-sun' ></i>
               <span class="links_name">Meteo du jour</span>
             </a>
             <span class="tooltip">Meteo du jour</span>
           </li>
-          <li>
-            <a href="favoris.html">
-              <i class='bx bxs-star' ></i>
-              <span class="links_name">Favoris</span>
-            </a>
-            <span class="tooltip">Favoris</span>
-          </li>
         </ul>
-    </div>
     </div>
 
     <div class="row">
@@ -68,64 +56,16 @@
           <div class="box">
             <div class="info-weather">
               <div class="weather">
-                <img src="images/icons8-soleil-emoji-96.png">
-                  <p class="description">Sunny</span></p>
-                  <p class="temperature">16<span>°C</span></p>
-                  <p class="humidity">Humidité: 30%</p>
-                  <p class="wind">Vent: 20 km/h</p>
-                  <br>
+                <div class="others" id="main-weather-items">
+                  <h2 style="margin-top: 20px;">Selectionner une ville </h2>
+                </div>
+
               </div>
             </div>
           </div>
         </div>
-        <hr style="margin: 10px 0 15px 0;">
-        <h2 style="color:white;"> Prévisions </h2>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Mardi</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>
-        </div>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Mercredi</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>
-        </div>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Jeudi</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>
-        </div>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Vendredi</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>
-        </div>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Samedi</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>  
-        </div>
-        <div class="weather-forecast" id="weather-forecast">
-          <div class="weather-forecast-item">
-            <div class="day">Dimanche</div>
-            <div class="temp">Night - 25.6°C</div>
-            <div class="temp">Day - 35.6°C</div>
-          </div>
-        </div>
       </div>
-    </div>
-
-    <div class="column right">
+      <div class="column right">
       <h2 style="color:white;"> Température Interieure </h2>
         <?php 
           if($temps = $mysqli->query (
@@ -138,23 +78,22 @@
               echo "<p class = 'temp_int'> ". $temp['date'] ." : " . $temp['temp_int'] . "°C </p>";
             }
           }
-
-          
         ?>  
+      </div>
     </div>
     <script> 
       let btn = document.querySelector("#btn");
-        let sidebar = document.querySelector(".sidebar");
-        let searchBtn = document.querySelector(".bx-search");
+      let sidebar = document.querySelector(".sidebar");
+      let searchBtn = document.querySelector(".bx-search");
 
-        btn.onclick = function() {
+      btn.onclick = function() {
         sidebar.classList.toggle("active");
       }
+   
 
-      searchBtn.onclick = function() {
-        sidebar.classList.toggle("active");
-      }
     </script>
-    
+
+    <script src="scripts/script.js"></script>
+
   </body>
 </html>
